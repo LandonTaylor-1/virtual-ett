0917d086838e3f34d058f1db89545439582e3dfb
Landon Taylor <landon.autumn.taylor@gmail.com>
2019-09-28 15:03:35 -0500

    server removed

--- server/routes/pesLeftRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { list, show, create } = require('../controllers/pesLeftController');
-
-router.get('/pesleft', list);
-router.get('/pesleft/:name', show)
-router.post('/pesleft', create);
-
-module.exports = router;
--- server/routes/pesRightRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { list, show, create } = require('../controllers/pesRightController');
-
-router.get('/pesright', list);
-router.get('/pesright/:name', show)
-router.post('/pesright', create);
-
-module.exports = router;
